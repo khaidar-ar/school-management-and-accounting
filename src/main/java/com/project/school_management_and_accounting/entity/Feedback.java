7bc0d38345e7a91608cc107728bb10361e98dd6a
khaidar-ar <96arrijal@gmail.com>
2024-12-25 21:00:47 +0700

    feat : feedback entity

---
+++ src/main/java/com/project/school_management_and_accounting/entity/Feedback.java
@@ -0,0 +1,38 @@
+package com.project.school_management_and_accounting.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "feedback", indexes = {
+        @Index(name = "feedback", columnList = "uuid")
+})
+public class Feedback extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private User teacher;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private User student;
+
+}
