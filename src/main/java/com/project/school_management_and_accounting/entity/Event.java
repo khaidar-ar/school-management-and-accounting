ce46e30d1829764b2540d4e30b6cdeeaa5e461d7
khaidar-ar <96arrijal@gmail.com>
2024-12-25 20:19:02 +0700

    fix : delete unused dependency

--- src/main/java/com/project/school_management_and_accounting/entity/Event.java
+++ src/main/java/com/project/school_management_and_accounting/entity/Event.java
@@ -1,11 +1,7 @@
 package com.project.school_management_and_accounting.entity;
 
-import com.project.school_management_and_accounting.enums.Grade;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
