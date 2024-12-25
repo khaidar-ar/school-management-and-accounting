430c60dcfb1a6baf465cdb39c1af44a982548207
khaidar-ar <96arrijal@gmail.com>
2024-12-25 14:16:52 +0700

    inital commit

---
+++ src/main/java/com/project/school_management_and_accounting/AppServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.school_management_and_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/school_management_and_accounting/AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.school_management_and_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
