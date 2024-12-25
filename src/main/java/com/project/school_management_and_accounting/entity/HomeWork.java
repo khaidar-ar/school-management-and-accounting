47eb38730d0c15ceaa3c2fdfb256ac60ea615b84
khaidar-ar <96arrijal@gmail.com>
2024-12-25 21:20:49 +0700

    feat : homework entity

---
+++ src/main/java/com/project/school_management_and_accounting/entity/HomeWork.java
@@ -0,0 +1,41 @@
+package com.project.school_management_and_accounting.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "homework", indexes = {
+        @Index(name = "homework_secure_id", columnList = "uuid")
+})
+public class HomeWork extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private User teacher;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id", nullable = false)
+    private Section section;
+
+}
