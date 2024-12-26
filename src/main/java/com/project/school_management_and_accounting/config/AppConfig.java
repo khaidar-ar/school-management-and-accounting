cdb7e8a27cb30adde3a57abeb9ca1bae0adfe599
khaidar-ar <96arrijal@gmail.com>
2024-12-26 20:39:47 +0700

    feat(config) : add modelmapper bean

--- src/main/java/com/project/school_management_and_accounting/config/AppConfig.java
+++ src/main/java/com/project/school_management_and_accounting/config/AppConfig.java
@@ -1,8 +1,18 @@
 package com.project.school_management_and_accounting.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 
 }
