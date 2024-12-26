fadcf0c7dd1d370642d1292e95ff919c2b2625ab
khaidar-ar <96arrijal@gmail.com>
2024-12-26 20:27:01 +0700

    feat(enums) : add get method

--- src/main/java/com/project/school_management_and_accounting/enums/BloodGroup.java
+++ src/main/java/com/project/school_management_and_accounting/enums/BloodGroup.java
@@ -1,5 +1,18 @@
 package com.project.school_management_and_accounting.enums;
 
 public enum BloodGroup {
-    A,AB,B,O
+    A, AB, B, O;
+
+    public static BloodGroup getBG(String b) {
+        switch (b.toUpperCase()) {
+            case "A":
+                return BloodGroup.A;
+            case "B":
+                return BloodGroup.B;
+            case "AB":
+                return BloodGroup.AB;
+            default:
+                return BloodGroup.O;
+        }
+    }
 }
--- src/main/java/com/project/school_management_and_accounting/enums/Gender.java
+++ src/main/java/com/project/school_management_and_accounting/enums/Gender.java
@@ -1,5 +1,13 @@
 package com.project.school_management_and_accounting.enums;
 
 public enum Gender {
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    public static Gender getGender(String g) {
+        if (g.toUpperCase() == "FEMALE")
+            return Gender.FEMALE;
+        else {
+            return Gender.MALE;
+        }
+    }
 }
