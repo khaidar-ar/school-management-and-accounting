2e904b684e0796ba062ef3dc526ef15f1c2f55ad
khaidar-ar <96arrijal@gmail.com>
2024-12-26 19:39:55 +0700

    feat(service) : init user service

---
+++ src/main/java/com/project/school_management_and_accounting/service/UserService.java
@@ -0,0 +1,5 @@
+package com.project.school_management_and_accounting.service;
+
+public interface UserService {
+
+}
---
+++ src/main/java/com/project/school_management_and_accounting/service/impl/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.project.school_management_and_accounting.service.impl;
+
+public class UserServiceImpl {
+
+}
