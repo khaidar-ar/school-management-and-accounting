820b478a9ecab12c18fd0db37bf7889f194ec1dc
khaidar-ar <96arrijal@gmail.com>
2024-12-26 18:54:24 +0700

    feat(entity) : routine

---
+++ src/main/java/com/project/school_management_and_accounting/entity/Routine.java
@@ -0,0 +1,53 @@
+package com.project.school_management_and_accounting.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "routine", indexes = {
+        @Index(name = "routine_secure_id", columnList = "uuid")
+})
+public class Routine extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Lob
+    @Column(name = "file_path")
+    private Byte[] filePath;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "active", nullable = false, columnDefinition = "boolean default true")
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id", nullable = false)
+    private School school;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id", nullable = false)
+    private Section section;
+
+}
