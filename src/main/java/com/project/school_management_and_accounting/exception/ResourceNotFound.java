9d275af13f667a9ca90b2f9d8a209cfbe9ab4c98
khaidar-ar <96arrijal@gmail.com>
2024-12-26 21:34:05 +0700

    feat(exception) : create resource not found exception

---
+++ src/main/java/com/project/school_management_and_accounting/exception/ResourceNotFound.java
@@ -0,0 +1,9 @@
+package com.project.school_management_and_accounting.exception;
+
+public class ResourceNotFound extends RuntimeException {
+
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+
+}
